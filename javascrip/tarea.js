47235431fd579d4ce7183e5662eb08344bda8055
Domenica-Munoz <mmunozo2@est.ups.edu.ec>
2021-04-16 08:02:12 -0500

    https://github.com/Domenica-Munoz/Aplicacion-Distribuidos
    tarea

--- javascrip/tarea.js
+++ javascrip/tarea.js
@@ -11,13 +11,13 @@ function Datos() {
         if (this.readyState == 4 && this.status == 200) {
             info = JSON.parse(this.responseText);
             var imagen = info[0].flag;
-            document.getElementById("foto").innerHTML = "<img id='Tfoto' src='" + imagen + "'alt='foto pokemon'>";
+            document.getElementById("foto").innerHTML = "<img id='fotoo' src='" + imagen + "'alt='foto pais'>";
            ///--------------------------------------------------------------///
            var name = " <h3>Name</h3>"+info[0].name;
            document.getElementById("Name").innerHTML = name;
            ///---------------------------------------------------------------///
            
-            var tople = "<table id='tabla'><tr>";
+            var tople =" <h3>Tople</h3>"+ "<table id='tabla'><tr>";
             for (var i = 0; i < info[0].topLevelDomain.length; i++) {
                 tople += "<th class='tg-46ru'>tople " + (i + 1) + "</th>";
             }
@@ -29,7 +29,7 @@ function Datos() {
             document.getElementById("tople").innerHTML = tople;
            ///---------------------------------------------------------------///
            
-            var bloque = "<table id='tabla'><tr>";
+            var bloque ="<h3>Bloque</h3>"+ "<table id='tabla'><tr>";
             for (var i = 0; i < info[0].regionalBlocs[0].otherAcronyms.length; i++) {
                 bloque += "<th class='tg-46ru'>Bloque " + (i + 1) + "</th>";
             }
@@ -41,15 +41,15 @@ function Datos() {
             document.getElementById("Bloque").innerHTML = bloque;
 
         ///---------------------------------------------------------------------///
-         var alpha2Code= info[0].alpha2Code;
+         var alpha2Code="<h3>Alpha2Code</h3>"+info[0].alpha2Code;
          document.getElementById("Alpha2Code").innerHTML = alpha2Code;
 
          ///------------------------------------------------------------------///
-         var alpha3Code =info[0].alpha3Code
+         var alpha3Code ="<h3>Alpha3Code</h3>"+info[0].alpha3Code
          document.getElementById("Alpha3Code").innerHTML = alpha3Code;
          ///------------------------------------------------------------------///
 
-            var call ="<table id='tabla'><tr>";
+            var call ="<h3>call</h3>"+"<table id='tabla'><tr>";
             for (var i = 0; i < info[0].callingCodes.length; i++) {
                 call += "<th class='tg-46ru'>call " + (i + 1) + "</th>";
             }
@@ -60,7 +60,7 @@ function Datos() {
             call += "</tr></table>";
             document.getElementById("call").innerHTML = call;
           ///-----------------------------------------------------------------///
-          var spelling ="<table id='tabla'><tr>";
+          var spelling ="<h3>Spelling</h3>"+"<table id='tabla'><tr>";
             for (var i = 0; i < info[0].altSpellings.length; i++) {
                 spelling += "<th class='tg-46ru'>spelling " + (i + 1) + "</th>";
             }
@@ -71,19 +71,19 @@ function Datos() {
             spelling += "</tr></table>";
             document.getElementById("spelling").innerHTML = spelling;
           ///----------------------------------------------------------------///
-                var capital = info[0].capital;
+                var capital = "<h3>Capital</h3>"+info[0].capital;
                 document.getElementById("Capital").innerHTML = capital;
          ///----------------------------------------------------------------///
-          var demonym=info[0].demonym;
+          var demonym="<h3>Demonym</h3>"+info[0].demonym;
           document.getElementById("Demonym").innerHTML = demonym;
          ///----------------------------------------------------------------///
-         var area=info[0].area;
+         var area="<h3>Area</h3>"+info[0].area;
          document.getElementById("Area").innerHTML = area;
          ///----------------------------------------------------------------///
-         var gini=info[0].gini;
+         var gini="<h3>Gini</h3>"+info[0].gini;
          document.getElementById("Gini").innerHTML = gini;
          ///----------------------------------------------------------------///
-         var time ="<table id='tabla'><tr>";
+         var time ="<h3>Time</h3>"+"<table id='tabla'><tr>";
          for (var i = 0; i < info[0].timezones.length; i++) {
             time += "<th class='tg-46ru'>time " + (i + 1) + "</th>";
          }
@@ -94,7 +94,7 @@ function Datos() {
          time += "</tr></table>";
          document.getElementById("time").innerHTML = time;
          ///---------------------------------------------------------------///
-         var bord ="<table id='tabla'><tr>";
+         var bord ="<h3>bord</h3>"+"<table id='tabla'><tr>";
          for (var i = 0; i < info[0].borders.length; i++) {
             bord += "<th class='tg-46ru'>bord " + (i + 1) + "</th>";
          }
@@ -105,13 +105,13 @@ function Datos() {
          bord += "</tr></table>";
          document.getElementById("bord").innerHTML = bord;
          ///---------------------------------------------------------------///
-         var native = info[0].nativeName;
+         var native ="<h3>Native</h3>"+ info[0].nativeName;
          document.getElementById("Native").innerHTML=native;
          ///---------------------------------------------------------------///
-         var numeric = info[0].numericCode;
+         var numeric = "<h3>Numeric</h3>"+info[0].numericCode;
          document.getElementById("Numeric").innerHTML=numeric;
          ///--------------------------------------------------------------///
-         var bloque1 = "<table id='tabla'><tr>";
+         var bloque1 = "<h3>Bloque1</h3>"+"<table id='tabla'><tr>";
          for (var i = 0; i < info[0].regionalBlocs[0].otherNames.length; i++) {
              bloque1 += "<th class='tg-46ru'>Bloque1 " + (i + 1) + "</th>";
          }
@@ -122,7 +122,7 @@ function Datos() {
          bloque1 += "</tr></table>";
          document.getElementById("Bloque1").innerHTML = bloque1;
          ///-------------------------------------------------------------///
-         var lat ="<table id='tabla'><tr>";
+         var lat ="<h3>Latlng</h3>"+"<table id='tabla'><tr>";
          for (var i = 0; i < info[0].latlng.length; i++) {
             lat += "<th class='tg-46ru'>lat " + (i + 1) + "</th>";
          }
@@ -133,47 +133,59 @@ function Datos() {
          lat += "</tr></table>";
          document.getElementById("lat").innerHTML = lat;
          ///--------------------------------------------------------------------////
-          var region = info[0].region;
+          var region = "<h3>Region</h3>"+info[0].region;
           document.getElementById("Region").innerHTML = region;
           ///-----------------------------------------------------------------///
           var subregion = info[0].subregion
           document.getElementById("Subregion").innerHTML = subregion;
           ///----------------------------------------------------------------///
-          var population = info[0].population
+          var population = "<h3>Population</h3>"+info[0].population
           document.getElementById("Population").innerHTML = population;
          ///----------------------------------------------------------------///
-         var cioc = info[0].cioc;
+         var cioc ="<h3>cioc</h3>"+ info[0].cioc;
          document.getElementById("Cioc").innerHTML=cioc;
          ///---------------------------------------------------------------///
-         var iso639_1= info[0].languages[0].iso639_1;
+         var code= "<h3>code</h3>"+info[0].currencies[0].code;
+         document.getElementById("concu").innerHTML=code;
+         var name= "<h3>name</h3>"+info[0].currencies[0].name;
+         document.getElementById("concu1").innerHTML=name;
+         var symbol= "<h3>symbol</h3>"+info[0].currencies[0].symbol;
+         document.getElementById("concu2").innerHTML=symbol;
+         ///---------------------------------------------------------------///
+         var iso639_1= "<h3>iso639_1</h3>"+ info[0].languages[0].iso639_1;
          document.getElementById("leng").innerHTML=iso639_1;
-         var iso639_2= info[0].languages[0].iso639_2;
+         var iso639_2= "<h3>iso639_2</h3>"+info[0].languages[0].iso639_2;
          document.getElementById("leng1").innerHTML=iso639_1;
-         var name= info[0].languages[0].name;
+         var name= "<h3>name</h3>"+ info[0].languages[0].name;
          document.getElementById("leng2").innerHTML=name;
-         var nativeName= info[0].languages[0].nativeName;
+         var nativeName= "<h3>nativeName</h3>"+info[0].languages[0].nativeName;
          document.getElementById("leng3").nativeName;
         ///----------------------------------------------------------------///
-        var de=info[0].translations.de;
+        var de="<h3>de</h3>"+info[0].translations.de;
         document.getElementById("Trans").innerHTML=de;
-        var es=info[0].translations.es;
+        var es="<h3>es</h3>"+info[0].translations.es;
         document.getElementById("Trans1").innerHTML=es;
-        var fr=info[0].translations.fr;
+        var fr="<h3>fr</h3>"+info[0].translations.fr;
         document.getElementById("Trans2").innerHTML=fr;
-        var ja=info[0].translations.ja;
+        var ja="<h3>ja</h3>"+info[0].translations.ja;
         document.getElementById("Trans3").innerHTML=ja;
-        var it=info[0].translations.it;
+        var it="<h3>it</h3>"+info[0].translations.it;
         document.getElementById("Trans4").innerHTML=it;
-        var br=info[0].translations.br;
+        var br="<h3>br</h3>"+info[0].translations.br;
         document.getElementById("Trans5").innerHTML=br;
-        var pt=info[0].translations.pt;
+        var pt="<h3>pt</h3>"+info[0].translations.pt;
         document.getElementById("Trans6").innerHTML=pt;
-        var nl=info[0].translations.nl;
+        var nl="<h3>nl</h3>"+info[0].translations.nl;
         document.getElementById("Trans7").innerHTML=nl;
-        var hr=info[0].translations.hr;
+        var hr="<h3>hr</h3>"+info[0].translations.hr;
         document.getElementById("Trans8").innerHTML=hr;
-        var fa=info[0].translations.fa;
+        var fa="<h3>fa</h3>"+info[0].translations.fa;
         document.getElementById("Trans9").innerHTML=fa;
+        ///-----------------------------------------------------///
+        var acronym ="<h3>acronym</h3>"+info[0].regionalBlocs[0].acronym;
+        document.getElementById("regional").innerHTML=acronym;
+        var name ="<h3>name</h3>"+info[0].regionalBlocs[0].name;
+        document.getElementById("regional1").innerHTML=name;
         
     }
     };
